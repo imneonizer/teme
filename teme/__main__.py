9284cf945a62c64bfaf66ada08124dab94007c1f
Nitin Rai <mneonizer@gmail.com>
2020-03-24 17:44:54 +0530

    Add files via upload

--- teme/__main__.py
+++ teme/__main__.py
@@ -9,12 +9,16 @@ ap.add_argument("-m", "--message", required=True,
 ap.add_argument("-t", "--token", required=False, default='None',
     help="pushit bot token address")
 ap.add_argument("-a", "--address", required=False,
-    default="https://gist.githubusercontent.com/imneonizer/90023d04d1f60d8f54e9c3b473d5b335/raw/f94843afe90b2da4a991c190e5bf5bf787a571ab/tokens.json",
+    default="https://gist.github.com/imneonizer/90023d04d1f60d8f54e9c3b473d5b335",
     help="pushit bot token address")
 args = vars(ap.parse_args())
 
 if args["token"] == 'None':
     args["token"] = get_token(args["address"])
 
-send_message(args["message"], args["token"])
+res = send_message(args["message"], args["token"])
+print(res)
+
+if str(res) != "<Response [200]>":
+    print("[Error] message not sent")
 
